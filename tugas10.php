c2c5c25dd97e8789887b047a8a644bfee114a70e
yohanes_prima <jooprima@gmail.com>
2019-10-07 11:00:32 +0700

    Tugas 10 PHP
    tugas ini dikumpulkan untuk niomic

---
+++ tugas10.php
@@ -0,0 +1,15 @@
+<?php
+$arr = array(1,3,8,2,5,7,4,0);
+
+print("Sebelum Di Sorting :<br>");
+print_r($arr);
+
+$arr = bubble_sort($arr);
+print("<br> Setelah Disorting (Buble Sort) :<br>");
+print_r($arr);
+
+
+function bubble_sort($arr) {
+        //Tulias Kode Kamu disini
+}
+?>
